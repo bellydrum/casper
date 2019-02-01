3fd01052cf16f152643f800f8d423ac732c00b8c
David Maness <davidmaness@Davids-MacBook-Pro.local>
2019-02-01 17:17:39 -0600

    add initial babel 7 packages

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
+#!/usr/bin/env node
+
+/**
+ * Entry point for casper.
+ */
+
 /** Import various tools. */
 const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+
+const debug = require('debug')('casper:server')
+const http = require('http')
+const port = normalizePort(process.env.PORT || '3000')
+
 
 /** Create app object. */
 const app = express()
@@ -45,4 +56,65 @@ app.use(function(err, req, res, next) {
   res.render('error')
 })
 
-module.exports = app
+const server = http.createServer(app)
+app.set('port', port)
+
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
+
+
+/**
+ * Helper functions.
+ */
+
+/** Normalize a port into a number, string, or false. */
+function normalizePort(val) {
+  const port = parseInt(val, 10)
+
+  if (isNaN(port)) {
+    // named pipe
+    return val
+  }
+
+  if (port >= 0) {
+    // port number
+    return port
+  }
+
+  return false
+}
+
+/** Event listener for HTTP server "error" event. */
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  const bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+      break
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use')
+      process.exit(1)
+      break
+    default:
+      throw error
+  }
+}
+
+/** Event listener for HTTP server "listening" event. */
+function onListening() {
+  const addr = server.address()
+  const bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port
+  debug('Listening on ' + bind)
+}
