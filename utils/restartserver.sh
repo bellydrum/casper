54aa4f1d41f83e805de1c7d64d7e836f0e9b307e
bellydrum <davidmaness1990@gmail.com>
2019-02-02 02:38:04 +0000

    sync restart script with dev repo

--- utils/restartserver.sh
+++ utils/restartserver.sh
@@ -6,11 +6,11 @@ if [ $PID ]
 then
     echo "Killing process $PID at port :3000."
 	kill $PID
-	~/casper/utils/restartserver.sh
+	~/development/casper/utils/restartserver.sh
 else
     echo "Restarting server. Please wait while the source code compiles..."
-	~/casper/node_modules/.bin/babel ~/casper/src -d ~/casper/dist
+	~/development/casper/node_modules/.bin/babel ~/development/casper/src -d ~/development/casper/dist
     wait
     echo "Server now listening on port 3000!"
-    node ~/casper/dist/server.js > /dev/null &
+    node ~/development/casper/dist/server.js > /dev/null &
 fi
